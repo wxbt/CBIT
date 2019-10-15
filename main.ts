267875a706d00f5a96549f61dc44f53475012c8d
wxbt <50980203+wxbt@users.noreply.github.com>
2019-10-15 11:21:05 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1028,4 +1028,84 @@ namespace RC-小车 {
         switch (direct) {
             case enPos.LeftState: {
                 if (pins.analogReadPin(AnalogPin.P1) < 500) {
-                    if (value == enLineState.
+                    if (value == enLineState.White) {
+                        temp = true;
+                    }
+                    setPwm(7, 0, 4095);
+                }
+                else {
+                    if (value == enLineState.Black) {
+                        temp = true;
+                    }
+                    setPwm(7, 0, 0);
+                }
+                break;
+            }
+
+            case enPos.RightState: {
+                if (pins.analogReadPin(AnalogPin.P2) < 500) {
+                    if (value == enLineState.White) {
+                        temp = true;
+                    }
+                    setPwm(6, 0, 4095);
+                }
+                else {
+                    if (value == enLineState.Black) {
+                        temp = true;
+                    }
+                    setPwm(6, 0, 0);
+                }
+                break;
+            }
+        }
+        return temp;
+
+    }
+    //% blockId=rc_CarCtrl block="小车控制|%index"
+    //% weight=93
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrl(index: CarState): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(255); break;
+            case CarState.Car_Back: Car_back(255); break;
+            case CarState.Car_Left: Car_left(255); break;
+            case CarState.Car_Right: Car_right(255); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(255); break;
+            case CarState.Car_SpinRight: Car_spinright(255); break;
+        }
+    }
+    //% blockId=rc_CarCtrlSpeed block="小车控制|%index|速度 %speed"
+    //% weight=92
+    //% blockGap=10
+    //% speed.min=0 speed.max=255
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrlSpeed(index: CarState, speed: number): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(speed); break;
+            case CarState.Car_Back: Car_back(speed); break;
+            case CarState.Car_Left: Car_left(speed); break;
+            case CarState.Car_Right: Car_right(speed); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(speed); break;
+            case CarState.Car_SpinRight: Car_spinright(speed); break;
+        }
+    }
+    //% blockId=rc_AloneCtrlSpeed block="单独电机|%index|速度 %speed"
+    //% weight=91
+    //% blockGap=10
+    //% speed.min=0 speed.max=255
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function AloneCtrlSpeed(index: AloneState, speed: number): void {
+        switch (index) {
+            case AloneState.Right_Z_Motor: Right_Z_run(speed); break;
+            case AloneState.Right_F_Motor: Right_F_run(speed); break;
+            case AloneState.Left_Z_Motor: Left_Z_run(speed); break;
+            case AloneState.Left_F_Motor: Left_F_run(speed); break;
+        }
+    }    
+}
